5fd0ca82536d265881158c206ed0adc7cbf7a162
Aimée Ogier d'Ivry <adesaintvenant@gmail.com>
2017-01-02 22:37:35 +0100

    ok

--- src/ApiWeight.ino
+++ src/ApiWeight.ino
@@ -1,5 +1,5 @@
 // API WEIGHT // adesaintvenant@gmail.com // 2016-2017 //
-// Merci à Objenious & Bouygues Telecom //
+// Merci ï¿½ Objenious & Bouygues Telecom //
 //Hardware : TheAirBoard.cc ; DHT11 ; //
 
 
@@ -9,6 +9,7 @@
 
 #include <Arduino.h>
 #include <dht11.h>
+#include <HX711.h>
 #include <TheAirBoard.h>
 #include <arm.h> //ATIM library for LoRaWAN connection
 #include <SoftwareSerial.h> // More information at https://www.arduino.cc/en/Reference/SoftwareSerial
@@ -21,12 +22,14 @@ SoftwareSerial mySerial(10, 11); // Pin 10 will work as TX and Pin 11 as RX
 // Define
 // ---------------------------------------------------------------------
 
-#define DHT11PIN 3
+#define DHT11PIN  3
 #define PHOTOCELL 4
-#define GREEN	   5               // GREEN dimmable LED
-#define BLUE	   6               // BLUE dimmable LED
-#define RED      9               // RED dimmable LED
-#define VBAT     A7
+#define GREEN	    5               // GREEN dimmable LED
+#define BLUE	    6               // BLUE dimmable LED
+#define RED       9               // RED dimmable LED
+#define HX711SCK  12
+#define HX711DT   13
+#define VBAT      A7
 
 // ---------------------------------------------------------------------
 // Global variables
@@ -34,17 +37,18 @@ SoftwareSerial mySerial(10, 11); // Pin 10 will work as TX and Pin 11 as RX
 dht11 DHT11;
 TheAirBoard board;
 Arm Objenious;
-
+HX711 scale(HX711DT,HX711SCK);
 
 volatile boolean f_wdt = true;
 int timeout = 0;
 
-byte msgg[4];
+byte msgg[7];
 
 int count = 1;
 int Temperature;
 int Humidity;
 int Luminosity;
+int Weight;
 float Battery;
 
 // ---------------------------------------------------------------------
@@ -58,9 +62,10 @@ void setup()
   board.setWatchdog(8000);
   mySerial.println("Setup OK");
 
-// ---------------------------------------------------------------------
-// LoRaWAN module Init and configuration
-// ---------------------------------------------------------------------
+ //Setup of scale
+ scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+ scale.tare();	 // This line reset the reading to 0
+
 
   //Init of the LoRaWAN module - Red light if error, Green light if Ok
   if(Objenious.Init(&Serial) != ARM_ERR_NONE)
@@ -128,29 +133,37 @@ void loop()
       Humidity = DHT11.humidity;
       Temperature = DHT11.temperature;
       Luminosity = analogRead(PHOTOCELL)/8; // Divided by 8 For 0 -> 255 range
+      Weight = -scale.get_units(5)*10;
       Battery = (3.3*analogRead(VBAT)/1024 + 1.2)*10; // Battery x10 (en dV)
 
       mySerial.print(Temperature);
       mySerial.print(Humidity);
       mySerial.print(Luminosity);
+      mySerial.print(Weight);
       mySerial.println(Battery);
+
+      LedDisplay(Weight);
 
       msgg[0] = 0;
       msgg[1] = (byte) Temperature;
       msgg[2] = (byte) Humidity;
       msgg[3] = (byte) Luminosity;
-      msgg[4] = (byte) Battery;
+      msgg[4] = (byte) (Weight>>8);
+      msgg[5] = (byte) Weight;
+      msgg[6] = (byte) Battery;
 
       mySerial.print(msgg[1]);
       mySerial.print(msgg[2]);
       mySerial.print(msgg[3]);
-      mySerial.println(msgg[4]);
+      mySerial.print(msgg[4]);
+      mySerial.print(msgg[5]);
+      mySerial.println(msgg[6]);
 
       Objenious.Send(msgg, sizeof(msgg));               // Send the msgg to Objenious network
       mySerial.println("Msgg sent");
       Blink(GREEN,100);
 
-      LedDisplay(count);
+      //LedDisplay(count);
       count++;
           timeout=0;
     }
@@ -160,7 +173,7 @@ void loop()
 }
 
 
-ISR(WDT_vect) {f_wdt = !f_wdt;}
+ISR(WDT_vect) {f_wdt = !f_wdt;} //Interupt instruction for deep sleep
 
 
 void LedDisplay(int Valeur)
